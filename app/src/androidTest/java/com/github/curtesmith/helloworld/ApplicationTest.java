a70157b0d2fe47597290d1b96ec78aa04898afbc
curtesmith <curt.e.smith@gmail.com>
2015-09-20 19:51:58 -0400

    First test

--- app/src/androidTest/java/com/github/curtesmith/helloworld/ApplicationTest.java
+++ app/src/androidTest/java/com/github/curtesmith/helloworld/ApplicationTest.java
@@ -3,11 +3,15 @@ package com.github.curtesmith.helloworld;
 import android.app.Application;
 import android.test.ApplicationTestCase;
 
+import junit.framework.Assert;
+
 /**
  * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
  */
 public class ApplicationTest extends ApplicationTestCase<Application> {
     public ApplicationTest() {
         super(Application.class);
+
+        assertTrue("what is going on?", false);
     }
 }
